bd7a6af346a9345cb1469746f5b96518fb895975
Nova_Adi <95131750+Adittya-Gupta@users.noreply.github.com>
2024-10-31 12:48:37 +0530

    first commit

---
+++ misc/cvt_to_lowercase.py
@@ -0,0 +1,22 @@
+#makeshift
+directories = ['data/few-nerd/inter/train.txt', 'data/few-nerd/intra/train.txt']
+
+for file1 in directories:
+  final_str = ""
+  cnt = 0
+  with open(file1, 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+      cnt+=1
+      print(cnt, end='\r')
+      cmpnnts = line.split('\t')
+      if len(cmpnnts) < 2:
+        final_str += line 
+        continue
+      c1, c2 = cmpnnts
+      c1 = c1.lower()
+      final_str += (c1 + '\t' + c2 )
+
+  f = open(file1, 'w')
+  f.write(final_str)
+  f.close()
